c9afc6c68d408f55b45c9a5c7ff33eb9dcd24320
LIHYHong <443484208@qq.com>
2019-07-17 09:51:37 +0800

    完善datepick

--- src/components/hy-datePicker/index.vue
+++ src/components/hy-datePicker/index.vue
@@ -12,9 +12,9 @@
 		props: {},
 		data() {
 			return {
-				newDate: '2019-07-16',
+				newDate: '2019-08-16',
 				arrDate: [],
-				arrType:'aaaa'
+				arrType: 'aaaa'
 			};
 		},
 		mounted() {
@@ -31,81 +31,80 @@
 				var newDay = new Date(a).getDate() < 10 ? '0' + new Date(a).getDate() : new Date(a).getDate();
 				var day = this.getCountDays(a);
 				var week = new Date(newYear + '-' + newMonth).getDay();
-				var o=Number(newMonth)==1?(newYear-1) + '-12':(newYear+'-'+(Number(newMonth)-1))
+				var o = Number(newMonth) == 1 ? (newYear - 1) + '-12' : (newYear + '-' + (Number(newMonth) - 1))
 				var oldDay = this.getCountDays(o);
-				// var oldDay = this.getCountDays(newYear + '-' + (Number(newMonth) - 1));
 				var lastweek = new Date(newYear + '-' + newMonth + '-' + day).getDay() + 1;
 				this.newWeek(newWeek, week, day, newDay, oldDay, lastweek);
 			},
-			lastMonth(data,day){
+			lastMonth(data, day) {
 				var newYear = new Date(data).getFullYear();
-				
-				var newMonth = (new Date(data).getMonth() ) < 10 ? '0' + (new Date(data).getMonth()) : (new Date(data).getMonth());
-				var o=Number(newMonth)==0?(newYear-1) + '-12-'+day:(newYear+'-'+newMonth+'-'+(day<10?'0'+day:day))
-
-			
+				var newMonth = (new Date(data).getMonth()) < 10 ? '0' + (new Date(data).getMonth()) : (new Date(data).getMonth());
+				var o = Number(newMonth) == 0 ? (newYear - 1) + '-12-' + day : (newYear + '-' + newMonth + '-' + (day < 10 ? '0' +
+					day : day))
 				return (o)
 			},
-			futureMonth(data,day){
+			futureMonth(data, day) {
 				var newYear = new Date(data).getFullYear();
-				var newMonth = (new Date(data).getMonth()+2 ) < 10 ? '0' + (new Date(data).getMonth()+2) : (new Date(data).getMonth()+2);
-				return (newYear+'-'+newMonth+'-'+(day<10?'0'+day:day))
+				var newMonth = (new Date(data).getMonth() + 2) < 10 ? '0' + (new Date(data).getMonth() + 2) : (new Date(data).getMonth() +
+					2);
+				var o = Number(newMonth) == 11 ? (newYear + 1) + '-01-' + (day < 10 ? '0' + day : day) : (newYear + '-' + newMonth +
+					'-' + (day < 10 ? '0' + day : day))
+				return (o)
 			},
 			newWeek(newWeek, week, day, newDay, oldDay, lastweek) {
-				var last = lastweek == 7 ? 0 : (7 - lastweek);
+				week = week == 0 ? 7 : week;
+				var last = lastweek == 7 ? 7 : (7 - lastweek);
 				var a = 7 - week;
 				var arr = [];
 				var c = oldDay - week;
 				var days = 1;
+				this.arrType = newDay;
 				for (var i = 1; i < (day + week + 1); i++) {
 					if (oldDay == c) {
 						arr.push({
-							i:days,
-							type:'new',
-							active:days==newDay?'aaaa':'',
-							
+							i: days,
+							type: 'new',
+							active: days == newDay ? newDay : '',
 						})
 						days = days + 1
 					} else {
 						c = c + 1;
 						arr.push({
-							i:c,
-							type:'last'
+							i: c,
+							type: 'last'
 						})
 					}
 				}
 				for (var j = 0; j < last; j++) {
 					arr.push({
-							i:j+1,
-							type:'future'
-						});
+						i: j + 1,
+						type: 'future'
+					});
 				}
 				this.arrDate = arr;
 			},
 			getCountDays(a = (new Date())) {
-				var curDate = new Date(a);
-				var curMonth = curDate.getMonth();
-				curDate.setMonth(curMonth + 1);
-				curDate.setDate(0);
-				return curDate.getDate();
+				var year = new Date(a).getFullYear();
+				var month = new Date(a).getMonth() + 1;
+				let days = new Date(year, month, 0).getDate()
+				return days;
 			},
-			activeDate(item){
-				if(item.type=='new'){
-					this.arrType=item.i;
-					item.active=item.i;
-				}else if(item.type=='last'){
-					this.newDate=this.lastMonth(this.newDate,item.i);
-				}else if(item.type=='future'){
-					this.newDate=this.futureMonth(this.newDate,item.i);
+			activeDate(item) {
+				if (item.type == 'new') {
+					this.arrType = item.i;
+					item.active = item.i;
+				} else if (item.type == 'last') {
+					this.newDate = this.lastMonth(this.newDate, item.i);
+				} else if (item.type == 'future') {
+					this.newDate = this.futureMonth(this.newDate, item.i);
 				}
 			}
 
 		},
 		computed: {},
 		watch: {
-			newDate(value){
+			newDate(value) {
 				this.newDateFn(value)
-
 			}
 		},
 		components: {},
@@ -115,26 +114,30 @@
 </script>
 
 <style>
-	*{
+	* {
 		margin: 0;
 		padding: 0;
 	}
-	.a{
+
+	.a {
 		width: 700px;
 	}
-	.b{
+
+	.b {
 		width: 100px;
 		height: 100px;
 		display: inline-block;
 		text-align: center;
 		line-height: 100px;
 	}
-	.b-last{
-		color:#c9c9c9
+
+	.b-last {
+		color: #c9c9c9
 	}
-	.b-b{
+
+	.b-b {
 		background: red;
 		border-radius: 50%;
-		
+
 	}
 </style>
